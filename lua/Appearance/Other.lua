042f504058f32e310f6e155ab18b90aadd966d05
jiangyz7cc <jiangyz7cc@outlook.com>
2024-03-26 10:41:15 +0800

    u

---
+++ lua/Appearance/Other.lua
@@ -0,0 +1,17 @@
+local o = vim.opt
+
+-- 背景：
+	-- 颜色：
+	o.background = "dark"
+
+-- 命令行：
+	-- 高：
+	o.cmdheight = 1
+
+-- 终端：
+	-- 界面颜色：
+	o.termguicolors = true
+
+-- 设置程序窗口的标题：显示当前程序路径
+	o.title = true
+
---
+++ lua/Appearance/RowCol.lua
@@ -0,0 +1,16 @@
+local o = vim.opt
+
+-- 行：
+	-- 号：
+	o.number = true
+	o.numberwidth = 4
+
+	-- 相对号：
+	o.relativenumber = true
+
+-- 列：
+	-- 标记列：
+	o.signcolumn = "yes"
+
+	-- 颜色列：
+	-- o.colorcolumn = "80"
