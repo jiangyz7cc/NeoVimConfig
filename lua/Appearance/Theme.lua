73387b17e12e53d2ab16dff3bb6eb1baa65d2845
jiangyz7cc <jiangyz7cc@outlook.com>
2024-04-08 11:10:56 +0800

    u

--- lua/Appearance/Theme.lua
+++ lua/Appearance/Theme.lua
@@ -3,13 +3,16 @@
 local cmd = vim.cmd
 
 --[[
-	可选列表：
-	carbonfox
-	dawnfox
-	terafox
-	nightfox
-	slate
-	-- tokyonight
+	nightfox系列：
+		carbonfox
+		dawnfox
+		terafox
+		nightfox
+		slate
+	
+	tokyonight系列：
+		tokyonight-day
+		tokyonight-night
 --]]
 
 cmd("colorscheme carbonfox")
--- lua/Extension/lazy/Extension/Config.lua
+++ lua/Extension/lazy/Extension/Config.lua
@@ -1,36 +1,45 @@
 return
 {
 	-- 编辑：
-	-- { "tpope/vim-repeat" } -- 启用对于 . 的重复执行
-	-- { "tpope/vim-surround" } -- 自动添加环绕字符：( , < , "
+	-- { "tpope/vim-repeat" } , -- 启用对于 . 的重复执行
+	-- { "tpope/vim-surround" } , -- 自动添加环绕字符：( , < , "
 
 	-- 键：
-    { "folke/which-key.nvim" , cmd = "WhichKey" }, -- 显示键映射
+    { "folke/which-key.nvim" , cmd = "WhichKey" } , -- 显示键映射
 
     -- 文件：
-    -- { "nvim-neo-tree/neo-tree.nvim" },
+    -- { "nvim-neo-tree/neo-tree.nvim" } ,
     { "nvim-tree/nvim-tree.lua" } ,
-	-- { "simpledanro/rename.vim "} -- 在缓冲区中重命名文件
+
+	-- 在缓冲区中重命名文件：
+	-- { "simpledanro/rename.vim "} ,
 
 	-- 语言：
 
 	-- LSP:
-	{ "williamboman/mason.nvim" } ,
-	{ "williamboman/mason-lspconfig.nvim" } ,
+
+	-- 配置：
 	{ "neovim/nvim-lspconfig" } ,
-	-- { "razzmatazz/csharp-language-server" } ,
+
+	-- 扩展管理：
+	{ "williamboman/mason.nvim" } ,
+
+	-- 连接 mason 和 nvim-lspconfig：
+	{ "williamboman/mason-lspconfig.nvim" } ,
+
+	-- LSP$
 
     -- 补全：
 	{ "hrsh7th/nvim-cmp" } ,
 	{ "hrsh7th/cmp-nvim-lsp" } ,
-	{ "hrsh7th/cmp-path" } ,
-	{ "hrsh7th/cmp-buffer" } ,
-	{ "hrsh7th/cmp-cmdline" } ,
+	-- { "hrsh7th/cmp-path" } ,
+	-- { "hrsh7th/cmp-buffer" } ,
+	-- { "hrsh7th/cmp-cmdline" } ,
 
 	-- 片段：
-	{ "hrsh7th/vim-vsnip" } , -- 引擎
-	{ "hrsh7th/cmp-vsnip" } , -- 补全
-	{ "L3MON4D3/LuaSnip" } ,
+	-- { "hrsh7th/vim-vsnip" } , -- 引擎
+	-- { "hrsh7th/cmp-vsnip" } , -- 补全
+	-- { "L3MON4D3/LuaSnip" } ,
 	{ "saadparwaiz1/cmp_luasnip" } ,
 	{ "rafamadriz/friendly-snippets" } ,
 
@@ -39,29 +48,23 @@ return
 	-- { "akinsho/bufferline.nvim" } ,
 	-- { "lewis6991/gitsigns.nvim" }
 
-	-- { 'nvim-lua/plenary.nvim' } ,
-	-- { 'nvim-telescope/telescope.nvim' } ,
-
 	-- 语法：
-	{ "nvim-treesitter/nvim-treesitter" , cmd = "TreeSitter" } ,
+	{ "nvim-treesitter/nvim-treesitter" } ,
 
 	-- 语言$
 
 	-- 导航：
 
-	-- 多域：
-    {
-		'nvim-telescope/telescope.nvim' ,
-		tag = '0.1.6' ,
-		dependencies = { 'nvim-lua/plenary.nvim' }
-    } ,
-
 	-- 文本：
 	{
-		'smoka7/hop.nvim' ,
-		version = "*" ,
-		opts = {} ,
+		"smoka7/hop.nvim"
 	} ,
+
+	-- 多域：（文件、缓冲区 ...）
+    {
+		"nvim-telescope/telescope.nvim" ,
+		dependencies = { "nvim-lua/plenary.nvim" }
+    } ,
 
 	-- 导航$
 
@@ -69,15 +72,20 @@ return
     -- { "folke/neoconf.nvim", cmd = "NeoConf" },
     -- { "folke/neodev.nvim" },
 
+	-- 外观：
+
     -- 图标：
 	{ "nvim-tree/nvim-web-devicons" } ,
-    {
-        'nvim-lualine/lualine.nvim' ,
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    } ,
+
+	-- 状态栏：
+	{
+		"nvim-lualine/lualine.nvim" ,
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+	},
 
     -- 主题：
-    { "EdenEast/nightfox.nvim" }
-    -- { "folke/tokyonight.nvim" }
+    { "EdenEast/nightfox.nvim" },
+    { "folke/tokyonight.nvim" }
 
+	-- 外观$
 }
