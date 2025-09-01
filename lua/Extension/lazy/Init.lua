cd0daa51417388f537f5c9663d422f07500b1545
jiangyz7cc <jiangyz7cc@outlook.com>
2025-09-01 23:54:13 +0800

    .

--- lua/Extension/lazy/Init.lua
+++ lua/Extension/lazy/Init.lua
@@ -1,21 +1,33 @@
--- print("lazy:")
+print("lazy:")
 
 -- 获取扩展 (lazy) 的目录路径：
 local edp = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
--- print("Extension's Dir Path:" .. edp)
+print("Extension's Dir Path:" .. edp)
 
 -- 如果 lazy 的目录路径不存在，则执行下载命令：
--- if not vim.loop.fs_stat(ep)
--- then
---     require("Extension/lazy/DownloadSelf").Download(ldp)
--- end
+if not vim.loop.fs_stat(edp)
+then
+	local module = require("Extension/lazy/DownloadSelf")
+
+	print("Download:")
+
+    module.Download(edp)
+
+	print("Download$")
+end
 
 vim.opt.rtp:prepend(edp)
 
 local e = require("lazy")
+
+if e == nil
+then
+	print("e = nil")
+end
+
 local c = require("Extension/lazy/ExtensionConfig")
 
 e.setup(c)
 
--- print("lazy$")
+print("lazy$")
--- lua/Preset/Init.lua
+++ lua/Preset/Init.lua
@@ -1,3 +1,5 @@
+vim.opt.runtimepath:append("~/.config/NeoVim")
+
 -- 停止使用 netrw：
 -- 将其加载的状态设置为已加载
 vim.g.loaded_netrw = 1
